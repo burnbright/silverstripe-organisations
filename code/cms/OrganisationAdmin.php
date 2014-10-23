05ce231b4b7c4c8af02d7efdcc47d79b1e7daa0e
Jeremy Shipman <jeremy@burnbright.net>
2014-10-23 21:05:49 +1300

    Added icon to OrganisationAdmin

--- code/cms/OrganisationAdmin.php
+++ code/cms/OrganisationAdmin.php
@@ -4,6 +4,7 @@ class OrganisationAdmin extends ModelAdmin{
 
 	private static $url_segment = "organisations";
 	private static $menu_title = "Organisations";
+	private static $menu_icon = 'organisations/images/building-icon.png';
 
 	private static $managed_models = array(
 		'Organisation'
