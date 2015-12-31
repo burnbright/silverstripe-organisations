d67d39e4573a8fa1319e519b508460e48c6d4ba7
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:27:22 +1300

    Converted to PSR-2

--- code/cms/OrganisationBulkLoader.php
+++ code/cms/OrganisationBulkLoader.php
@@ -1,41 +1,43 @@
 <?php
 
-class OrganisationBulkLoader extends CSVBulkLoader{
-	
-	public $columnMap = array(
-		'Organisation' => 'Name'
-	);
+class OrganisationBulkLoader extends CSVBulkLoader
+{
+    
+    public $columnMap = array(
+        'Organisation' => 'Name'
+    );
 
-	public $duplicateChecks = array(
-		'Organisation' => 'Name'
-   	);
-	
-	public function __construct($class) {
-		parent::__construct($class);
+    public $duplicateChecks = array(
+        'Organisation' => 'Name'
+    );
+    
+    public function __construct($class)
+    {
+        parent::__construct($class);
 
-		//map fields
-		$singular = singleton('Organisation')->singular_name();
-		$this->columnMap[$singular] = "Name";
-		$this->duplicateChecks[$singular] = "Name";
-		$singular = singleton('Organisation')->i18n_singular_name();
-		$this->columnMap[$singular] = "Name";
-		$this->duplicateChecks[$singular] = "Name";
-	}
+        //map fields
+        $singular = singleton('Organisation')->singular_name();
+        $this->columnMap[$singular] = "Name";
+        $this->duplicateChecks[$singular] = "Name";
+        $singular = singleton('Organisation')->i18n_singular_name();
+        $this->columnMap[$singular] = "Name";
+        $this->duplicateChecks[$singular] = "Name";
+    }
 
-	protected function processRecord($record, $columnMap, &$results, $preview = false) {
+    protected function processRecord($record, $columnMap, &$results, $preview = false)
+    {
 
-		//TODO: skip empties
+        //TODO: skip empties
 
-		$this->extend('preprocess', $record, $columnMap, $results, $preview);
-		$id = parent::processRecord($record, $columnMap, $results, $preview);
-		if($org = Organisation::get()->byID($id)){
-			$org->write();
-			//callback for doing other custom stuff
-			$this->extend('postprocess', $org, $record, $columnMap, $results, $preview);
-			$org->destroy();
-			unset($org);
-		}
-		return $id;
-	}
-
+        $this->extend('preprocess', $record, $columnMap, $results, $preview);
+        $id = parent::processRecord($record, $columnMap, $results, $preview);
+        if ($org = Organisation::get()->byID($id)) {
+            $org->write();
+            //callback for doing other custom stuff
+            $this->extend('postprocess', $org, $record, $columnMap, $results, $preview);
+            $org->destroy();
+            unset($org);
+        }
+        return $id;
+    }
 }
--- code/forms/CreateOrganisationForm.php
+++ code/forms/CreateOrganisationForm.php
@@ -1,68 +1,72 @@
 <?php
 
-class CreateOrganisationForm extends Form{
-	
-	
-	public function __construct($controller, $name = "CreateOrganisationForm") {
+class CreateOrganisationForm extends Form
+{
+    
+    
+    public function __construct($controller, $name = "CreateOrganisationForm")
+    {
+        $fields = $this->getOrganisationFields();
+        $actions = new FieldList(
+            FormAction::create("docreate", "Create")
+        );
 
-		$fields = $this->getOrganisationFields();
-		$actions = new FieldList(
-			FormAction::create("docreate", "Create")
-		);
+        $validator = new OrganisationFormValidator(
+            'Name'
+        );
 
-		$validator = new OrganisationFormValidator(
-			'Name'
-		);
+        parent::__construct($controller, $name, $fields, $actions, $validator);
+    }
 
-		parent::__construct($controller, $name, $fields, $actions, $validator);
-	}
+    public function getOrganisationFields()
+    {
+        return singleton("Organisation")->getOrganisationFormFields();
+    }
 
-	public function getOrganisationFields() {
-		return singleton("Organisation")->getOrganisationFormFields();
-	}
+    public function docreate($data, $form)
+    {
+        $organisation = new Organisation();
+        $form->saveInto($organisation);
+        $organisation->write();
 
-	public function docreate($data, $form){
-		$organisation = new Organisation();
-		$form->saveInto($organisation);
-		$organisation->write();
-
-		if($link = $organisation->getProfileLink('edit')){
-			return $this->controller->redirect($link);
-		}
-		return $this->controller->redirectBack();
-	}
+        if ($link = $organisation->getProfileLink('edit')) {
+            return $this->controller->redirect($link);
+        }
+        return $this->controller->redirectBack();
+    }
 }
 
-class OrganisationFormValidator extends RequiredFields{
+class OrganisationFormValidator extends RequiredFields
+{
 
-	protected $uniquefield = "Name";
+    protected $uniquefield = "Name";
 
-	public function php($data){
-		$valid = parent::php($data);
+    public function php($data)
+    {
+        $valid = parent::php($data);
 
-		$uniquefield = $this->uniquefield;
+        $uniquefield = $this->uniquefield;
 
-		$organisation = Organisation::get()
-			->filter($uniquefield, $data[$uniquefield])
-			->first();
+        $organisation = Organisation::get()
+            ->filter($uniquefield, $data[$uniquefield])
+            ->first();
 
-		if($uniquefield && is_object($organisation) && $organisation->isInDB()) {
-			$uniqueField = $this->form->Fields()->dataFieldByName($uniquefield);
-			$this->validationError(
-				$uniqueField->id(),
-				sprintf(
-					_t(
-						'Member.VALIDATIONORGANISATIONEXISTS',
-						'An organisation already exists with the same %s'
-					),
-					strtolower($uniquefield)
-				),
-				'required'
-			);
-			$valid = false;
-		}
+        if ($uniquefield && is_object($organisation) && $organisation->isInDB()) {
+            $uniqueField = $this->form->Fields()->dataFieldByName($uniquefield);
+            $this->validationError(
+                $uniqueField->id(),
+                sprintf(
+                    _t(
+                        'Member.VALIDATIONORGANISATIONEXISTS',
+                        'An organisation already exists with the same %s'
+                    ),
+                    strtolower($uniquefield)
+                ),
+                'required'
+            );
+            $valid = false;
+        }
 
-		return $valid;
-	}
-
+        return $valid;
+    }
 }
